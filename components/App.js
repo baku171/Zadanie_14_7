bcfe59ab58ab7048f77bed855ac41b2b3d3b2dd7
baku171 <pbak@esri.pl>
2017-11-06 10:04:28 +0100

    http server added

--- components/App.js
+++ components/App.js
@@ -1,20 +1,19 @@
-var contacts = [{
+var contacts = [
+    {
         id: 1,
         firstName: 'Jan',
         lastName: 'Nowak',
-        email: 'jan.nowak@example.com',
-    },
-    {
+        email: 'jan.nowak@example.com'
+    }, {
         id: 2,
         firstName: 'Adam',
         lastName: 'Kowalski',
-        email: 'adam.kowalski@example.com',
-    },
-    {
+        email: 'adam.kowalski@example.com'
+    }, {
         id: 3,
         firstName: 'Zbigniew',
         lastName: 'Koziol',
-        email: 'zbigniew.koziol@example.com',
+        email: 'zbigniew.koziol@example.com'
     }
 ];
 
@@ -25,11 +24,11 @@ var contactForm = {
 };
 
 var App = React.createClass({
-    render: function() {
-        return ( 
+    render: function () {
+        return (
             <div className={'app'}>
-                <ContactForm contact={contactForm}/> 
-                <Contacts items={contacts}/> 
+                <ContactForm contact={contactForm}/>
+                <Contacts items={contacts}/>
             </div>
         );
     }
--- components/Contact.js
+++ components/Contact.js
@@ -1,12 +1,14 @@
 var Contact = React.createClass({
     propTypes: {
-        item: React.PropTypes.object.isRequired,
+        item: React.PropTypes.object.isRequired
     },
 
-    render: function() {
+    render: function () {
         return (
             <div className={'contactItem'}>
-                <img className={'contactImage'} src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
+                <img
+                    className={'contactImage'}
+                    src={'http://icons.veryicon.com/ico/System/100%20Flat%20Vol.%202/contacts.ico'}/>
                 <p className={'contactLabel'}>
                     Imię: {this.props.item.firstName}
                 </p>
